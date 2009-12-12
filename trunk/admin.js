6dbd950903d7f2af7e66f6adeeb491b6ef96e8c5
36flavours <36flavours@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-12 15:02:35 +0000

    Fixed errors when error reporting is set to all. Fixed admin post/pages list display conflicting with other custom columns. Tested the plugin in WordPress 2.9.
    git-svn-id: https://plugins.svn.wordpress.org/subheading@182090 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/admin.js
+++ trunk/admin.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function($) {
 	$('.column-subheading').hide();
 	$('td.subheading').each(function() {
-		$('td.post-title :first', $(this).parents('tr'))
+		$('td.post-title:first', $(this).parents('tr'))
+			.children(':first')
 			.after($(this).html());
 	});
 });
--- trunk/settings.php
+++ trunk/settings.php
@@ -2,19 +2,19 @@
 if (array_key_exists('updated', $_GET)) :
 ?>
 <div id="message" class="updated fade">
-	<p>Settings saved.</p>
+	<p><?php _e('Settings saved'); ?>.</p>
 </div>
 <?php endif; ?>
 <div class="wrap">
 	<div id="icon-plugins" class="icon32"><br /></div>
-	<h2><?=$this->name?> Settings</h2>
+	<h2><?=$this->name?> <?php _e('Settings'); ?></h2>
 	<form method="post" action="options.php">
 		<?php settings_fields($this->tag.'_options'); ?>
 		<table class="form-table">
 			<tr valign="top">
 				<td>
 					<label for="subheading[rss]">
-						<input name="subheading[rss]" type="checkbox" id="subheading[rss]" value="1" <?php checked('1', $this->options['rss']); ?> />
+						<input name="subheading[rss]" type="checkbox" id="subheading[rss]" value="1" <?php if (array_key_exists('rss', $this->options)) { checked('1', $this->options['rss']); } ?> />
 						Append to RSS feeds.
 					</label>
 				</td>
@@ -22,15 +22,15 @@ if (array_key_exists('updated', $_GET)) :
 			<tr valign="top">
 				<td>
 					<label for="subheading[reposition]">
-						<input name="subheading[reposition]" type="checkbox" id="subheading[reposition]" value="1" <?php checked('1', $this->options['reposition']); ?> />
-						Prevent reposition of input under the title.
+						<input name="subheading[reposition]" type="checkbox" id="subheading[reposition]" value="1" <?php if (array_key_exists('reposition', $this->options)) { checked('1', $this->options['reposition']); } ?> />
+						Prevent reposition of input under the title when editing.
 					</label>
 				</td>
 			</tr>
 			<tr valign="top">
 				<td>
 					<label for="subheading[posts]">
-						<input name="subheading[posts]" type="checkbox" id="subheading[posts]" value="1" <?php checked('1', $this->options['posts']); ?> />
+						<input name="subheading[posts]" type="checkbox" id="subheading[posts]" value="1" <?php if (array_key_exists('posts', $this->options)) { checked('1', $this->options['posts']); } ?> />
 						Enable for posts as well as pages.
 					</label>
 				</td>
@@ -38,7 +38,7 @@ if (array_key_exists('updated', $_GET)) :
 			<tr valign="top">
 				<td>
 					<label for="subheading[lists]">
-						<input name="subheading[lists]" type="checkbox" id="subheading[lists]" value="1" <?php checked('1', $this->options['lists']); ?> />
+						<input name="subheading[lists]" type="checkbox" id="subheading[lists]" value="1" <?php if (array_key_exists('lists', $this->options)) { checked('1', $this->options['lists']); } ?> />
 						Display on admin edit lists.
 					</label>
 				</td>
@@ -46,7 +46,7 @@ if (array_key_exists('updated', $_GET)) :
 			<tr valign="top">
 				<td>
 					<label for="subheading[tags]">
-						<input name="subheading[tags]" type="checkbox" id="subheading[tags]" value="1" <?php checked('1', $this->options['tags']); ?> />
+						<input name="subheading[tags]" type="checkbox" id="subheading[tags]" value="1" <?php if (array_key_exists('tags', $this->options)) { checked('1', $this->options['tags']); } ?> />
 						Apply shortcode filters.
 					</label>
 				</td>
@@ -54,7 +54,7 @@ if (array_key_exists('updated', $_GET)) :
 			<tr valign="top">
 				<td>
 					<label for="subheading[tidy]">
-						<input name="subheading[tidy]" type="checkbox" id="subheading[tidy]" value="1" <?php checked('1', $this->options['tidy']); ?> />
+						<input name="subheading[tidy]" type="checkbox" id="subheading[tidy]" value="1" <?php if (array_key_exists('tidy', $this->options)) { checked('1', $this->options['tidy']); } ?> />
 						Remove all traces of plugin on deactivation.
 					</label>
 				</td>
