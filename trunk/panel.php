aaa8c97d473be10ee18f807fc0417c38486b3336
36flavours <36flavours@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-08 20:21:12 +0000

    Replaced all remaining PHP short tags.
    git-svn-id: https://plugins.svn.wordpress.org/subheading@203329 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/panel.php
+++ trunk/panel.php
@@ -2,7 +2,7 @@
 if (!isset($this->options['reposition'])) :
 ?><script type="text/javascript">
 	jQuery(function($) {
-		var subheading = $("#<?=$this->tag?>_postbox")
+		var subheading = $("#<?php echo $this->tag; ?>_postbox")
 			.hide()
 			.find("input")
 			.attr('tabindex', 1)
@@ -11,7 +11,7 @@ if (!isset($this->options['reposition'])) :
 </script>
 <?php endif; ?>
 <style type="text/css">
-	#wp_<?=$this->tag?> { width: 100%; padding: 5px; font-size: 13px; margin-top: 3px; }
+	#wp_<?php echo $this->tag; ?> { width: 100%; padding: 5px; font-size: 13px; margin-top: 3px; }
 </style>
-<?=wp_nonce_field('wp_'.$this->tag, $this->tag.'nonce')?>
-<input type="text" autocomplete="off" id="wp_<?=$this->tag?>" name="<?=$this->tag?>_value" value="<?=esc_html($this->value())?>" />
+<?php echo wp_nonce_field('wp_'.$this->tag, $this->tag.'nonce'); ?>
+<input type="text" autocomplete="off" id="wp_<?php echo $this->tag; ?>" name="<?php echo $this->tag; ?>_value" value="<?php echo esc_html($this->value()); ?>" />
