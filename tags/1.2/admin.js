7b9e8f3c0bdac8ac4f3f14ea230365009a49ed62
stvwhtly <stvwhtly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-25 22:32:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/subheading/tags/1.6.1@590380 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.2/admin.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.column-subheading').hide();
-	$('td.subheading').each(function() {
-		$('td.post-title :first', $(this).parents('tr'))
-			.after($(this).html());
-	});
-});
