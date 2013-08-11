9f6d3008764d6e25ac76ed13b9cd92dd02307f8e
stvwhtly <stvwhtly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-11 10:33:31 +0000

    Fixed a bug with the JavaScript used by the settings page, which would incorrectly toggle the auto append value.
    git-svn-id: https://plugins.svn.wordpress.org/subheading/trunk@754723 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.js
+++ admin.js
@@ -6,15 +6,9 @@ jQuery( document ).ready( function( $ ) {
 			.after($(this).html());
 	} );
 	$( '#subheading_append' ).click( function( e ) {
-		if ( e.setup ) {
-			e.preventDefault();
-		}
 		$( '#subheading_before, #subheading_after' ).parent().css(
 			'display',
 			( $( this ).is( ':checked' ) ? '' : 'none' )
 		);
-	} ).trigger( {
-		type: 'click',
-		setup: true 
-	} );
+	} ).triggerHandler( 'click' );
 } );
